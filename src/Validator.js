0d10943d06fa16a111a82a83ed4554838b888f5e
Matias Dumrauf <matias.dumrauf@gmail.com>
2017-12-12 01:53:19 -0300

    implemented Validator base class with tests

--- src/Validator.js
+++ src/Validator.js
@@ -1,8 +1,19 @@
 
 class Validator {
 
-    validate() {
+    constructor(errorMessage, args) {
+        this.errorMessage = errorMessage;
+        this.args = args;
+    }
 
+    validate(obj) {
+        if (!this.isValid(obj)) {
+            throw new Error(this.errorMessage);
+        }
+    }
+
+    isValid(obj) {
+        throw new Error('Must be implemented in subclass');
     }
 
 }
--- test/Validator.test.js
+++ test/Validator.test.js
@@ -1,22 +1,52 @@
 
 const should = require('should');
+const sinon = require('sinon');
 const Validator = require('../src/Validator');
 
 
 describe('Validator', () => {
 
+  let validator;
+
+  beforeEach(() => {
+     validator = new Validator('An error message', null);
+  });
+
+
   context('#validate', () => {
 
-    const validator = new Validator();
-
     it('should return undefined if validation passes', () => {
+
+      sinon.stub(validator, 'isValid').returns(true);
+
       should(validator.validate()).be.undefined();
+
+      validator.isValid.restore();
+
     });
 
     it('should throw Error if validation fails', () => {
+
+      sinon.stub(validator, 'isValid').returns(false);
+
       should(() => {
         validator.validate();
-      }).throw(Error);
+      }).throw('An error message');
+
+      validator.isValid.restore();
+
+    });
+
+  });
+
+  context('#isValid', () => {
+
+    it('should throw Error as default implementation', () => {
+
+      should(() => {
+        validator.isValid();
+      }).throw('Must be implemented in subclass');
+
     });
 
   });
