cdc9986e1d1a04a5583c220df57cf663571300bd
Alan Saracho <saracho.alan@gmail.com>
2017-12-18 13:40:51 -0300

    updated index exports

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import GreaterThan from './GreaterThanValidator';
+import LessThan from './LessThanValidator';
+import Empty from './EmptyValidator';
+import EqualTo from './EqualsToValidator';
+import NotEqualTo from './NotEqualsToValidator';
+
+export {
+    GreaterThan,
+    LessThan,
+    Empty,
+    EqualTo,
+    NotEqualTo
+};
