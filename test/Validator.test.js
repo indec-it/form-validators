042485402e680dcf8012b59c38c273f07d48c466
Matias Dumrauf <matias.dumrauf@gmail.com>
2018-03-04 00:58:09 -0300

    changed validate test case

--- test/Validator.test.js
+++ test/Validator.test.js
@@ -15,11 +15,13 @@ describe('Validator', () => {
 
   context('#validate', () => {
 
-    it('should return undefined if validation passes', () => {
+    it('should not throw Error if validation passes', () => {
 
       sinon.stub(validator, 'isValid').returns(true);
 
-      should(validator.validate()).be.undefined();
+      should(() => {
+        validator.validate();
+      }).not.throwError();
 
       validator.isValid.restore();
 
