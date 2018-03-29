30ab3ba08e720dc03cf845511f18902603dbf04b
Damián Berruezo <damianberruezo@gmail.com>
2018-03-28 23:33:07 -0300

    feature(hasBlockerState): check conditions blocking validation result

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,11 @@ module.exports = {
     lowerBoundaryType: {
         NO_LESS_THAN_ONE: 1,
         GREATER_THAN_ZERO: 2
+    },
+    validationStates: {
+        IGNORE: 0,
+        VALID: 0,
+        WARNING: 1,
+        ERROR: 2
     }
 };
